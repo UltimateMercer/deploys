e543489ff15b9232b4894483ecbf421b38bd53e4
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2021-11-01 15:12:45 -0300

    Nuxt config change

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,7 +97,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: { transpile: "vue-lazyload" },
+  build: { transpile: ["vue-lazyload"] },
 
   fontawesome: {
     icons: {
